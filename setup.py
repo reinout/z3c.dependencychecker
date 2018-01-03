923722d14f8ba8a0eccbc0d4eb30264ae5062f1f
Gil Forcada <gil.gnome@gmail.com>
2018-01-03 13:00:55 +0100

    [cleanup] Reformat setup.py

--- setup.py
+++ setup.py
@@ -10,52 +10,58 @@ long_description = '\n\n'.join([
     open('CHANGES.rst').read(),
     ])
 
-setup(name='z3c.dependencychecker',
-      version=version,
-      description="""Checks which imports are done and compares them to what's
-      in setup.py and warns when discovering missing or unneeded
-      dependencies.""",
-      long_description=long_description,
-      # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
-      classifiers=[
-          'Development Status :: 5 - Production/Stable',
-          'Environment :: Console',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: BSD License',
-          'Programming Language :: Python',
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Topic :: Software Development :: Quality Assurance',
-      ],
-      keywords=[
-          'dependencies',
-          'requirements',
-          'missing',
-          'imports',
-      ],
-      author='Reinout van Rees',
-      author_email='reinout@vanrees.org',
-      url='https://github.com/reinout/z3c.dependencychecker',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['z3c'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'stdlib-list',
-          ],
-      extras_require = {
-          'test': [
-              'pytest',
-              'mock',
-              ],
-          },
-      entry_points={
-          'console_scripts':
-          ['dependencychecker = z3c.dependencychecker.main:main'
-           ]},
-      )
+description = """
+Checks which imports are done and compares them to what's
+in setup.py and warns when discovering missing or unneeded
+dependencies.
+"""
+
+setup(
+    name='z3c.dependencychecker',
+    version=version,
+    description=description,
+    long_description=long_description,
+    # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Quality Assurance',
+    ],
+    keywords=[
+        'dependencies',
+        'requirements',
+        'missing',
+        'imports',
+    ],
+    author='Reinout van Rees',
+    author_email='reinout@vanrees.org',
+    url='https://github.com/reinout/z3c.dependencychecker',
+    license='BSD',
+    packages=find_packages(exclude=['ez_setup', ]),
+    namespace_packages=['z3c', ],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'stdlib-list',
+    ],
+    extras_require={
+        'test': [
+            'pytest',
+            'mock',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'dependencychecker = z3c.dependencychecker.main:main',
+        ]
+    },
+)
